0f00505e2eb9dc732522db71e42b65671e2550c5
rocky112358 <rocky112358@gmail.com>
2019-08-11 15:45:52 +0900

    ADD: 마우스 올린 유저의 screen name 알아내기

---
+++ memoOnCard.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", updateScreenName, false);
+
+function updateScreenName(evt) {
+    setInterval(getScreenNameOnCard, 100);
+
+    let currentScreenName = null;
+    let selectorString = "div.css-1dbjc4n div.css-1dbjc4n.r-yfoy6g.r-t23y2h.r-xnswec div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
+    function getScreenNameOnCard() {
+        if (document.querySelector(selectorString) !== null && 
+            currentScreenName !== document.querySelector(selectorString).getAttribute("href").slice(1)) {
+            console.log("mouse in", currentScreenName = document.querySelector(selectorString).getAttribute("href").slice(1))
+        } else if (document.querySelector(selectorString) === null && currentScreenName !== null) {
+            currentScreenName = null;
+            console.log("mouse out")
+        }
+    }
+}
