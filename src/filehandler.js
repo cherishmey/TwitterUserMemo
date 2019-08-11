a659feb3a01b795c835c84331216f7f8641cebc8
chorista <chorista01@gmail.com>
2019-08-11 16:27:02 +0900

    filehandler 테스트값 제거

--- src/filehandler.js
+++ src/filehandler.js
@@ -1,19 +1,21 @@
 let btn_save = document.getElementById('btn_save');
 let btn_load = document.getElementById('btn_load');
 
-let memo_zone = document.getElementById('memo');
-
-var items = {"test_user_id":{"memo" : "트친1", "tag" : "팦14"}};
-
-btn_save.addEventListener('click', function saveData(){
-    chrome.storage.sync.set(items, function(items) {
-        console.log(items+"이 저장되었습니다");
+function saveData(items){
+    btn_save.addEventListener('click', function save(){
+        chrome.storage.sync.set(items, function(items) {
+            console.log(items+"이 저장되었습니다");
+        });
     });
-});
+}
 
-btn_load.addEventListener('click', function loadData(){
-    chrome.storage.sync.get(["test_user_id"], function(result){
-        var value = result["test_user_id"].tag
-        memo_zone.innerHTML = "내 트친은 " + value +"입니다.";
+function loadData(key){
+    btn_load.addEventListener('click', function load(){
+        chrome.storage.sync.get([key], function(result){
+            var tag = result[key].tag
+            var memo = result[key].memo
+            console.log(key + ": { memo: " +memo +", tag: "
+            + tag +"}");
+        });
     });
-});
+}
