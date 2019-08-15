224b12045288052f7d29bee395de3c063447fb16
rocky112358 <rocky112358@gmail.com>
2019-08-15 18:19:43 +0900

    FIX: 배경색에 따라 작동하지 않던 문제 수정

--- src/memoOnCard.js
+++ src/memoOnCard.js
@@ -4,7 +4,7 @@ function updateScreenName(evt) {
     setInterval(getScreenNameOnCard, 100);
 
     let currentScreenName = null;
-    let selectorString = "div.css-1dbjc4n div.css-1dbjc4n.r-yfoy6g.r-t23y2h.r-xnswec div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
+    let selectorString = "div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
     function getScreenNameOnCard() {
         if (document.querySelector(selectorString) !== null && 
             currentScreenName !== document.querySelector(selectorString).getAttribute("href").slice(1)) {
