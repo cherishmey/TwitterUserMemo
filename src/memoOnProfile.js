ba1b6f489e4cae922d70962283226ca985bfc866
TimidLion <sgm02186@naver.com>
2019-08-19 22:49:09 +0900

    ADD: textarea on profile page

---
+++ src/memoOnProfile.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", updateScreenName, false);
+
+function updateScreenName(evt) {
+    setInterval(showTextareaOnProfile, 100);
+
+    let selectorString = "div.css-1dbjc4n.r-obd0qt.r-18u37iz.r-1w6e6rj.r-1h0z5md.r-dnmrzs"
+    let memoAdded = false
+    function showTextareaOnProfile() {
+        if (document.querySelector(selectorString) !== null && !memoAdded) {
+            console.log("Hello")
+            let textArea = document.createElement("textarea");
+            document.querySelector(selectorString).insertAdjacentElement("beforeBegin",textArea);
+            memoAdded = true
+        } else if (document.querySelector(selectorString) === null) {
+            console.log("why?")
+        }
+    }
+}
