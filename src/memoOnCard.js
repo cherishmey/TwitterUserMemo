7919cf367042c02c6c89be0ebea46b7458cd20b5
rocky112358 <rocky112358@gmail.com>
2019-08-22 22:35:43 +0900

    ADD: display name으로부터 user id 구해서 콘솔에 출력

--- src/memoOnCard.js
+++ src/memoOnCard.js
@@ -6,11 +6,21 @@ function updateScreenName(evt) {
     let currentScreenName = null;
     let selectorString = "div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
     function getScreenNameOnCard() {
-        if (document.querySelector(selectorString) !== null && 
+        if (document.querySelector(selectorString) !== null &&
             currentScreenName !== document.querySelector(selectorString).getAttribute("href").slice(1)) {
             console.log("mouse in", currentScreenName = document.querySelector(selectorString).getAttribute("href").slice(1))
+            fetch("https://us-central1-true-ability-250610.cloudfunctions.net/get-user-ids-from-display-names-usc",
+                {
+                    method: "POST",
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ display_names: [currentScreenName] })
+                })
+                .then(response => response.json()).then(data => console.log(data))
             var textArea = document.createElement("textarea");
-            document.querySelector(selectorString).insertAdjacentElement("afterEnd",textArea);
+            document.querySelector(selectorString).insertAdjacentElement("afterEnd", textArea);
         } else if (document.querySelector(selectorString) === null && currentScreenName !== null) {
             currentScreenName = null;
             console.log("mouse out")
