1107ff8b872c6c818363d1586e33403212495d90
rocky112358 <rocky112358@gmail.com>
2019-09-15 14:24:50 +0900

    FIX: API 요청 시 Bearer token과 csrf token을 포함시키도록 수정

--- js/memoOnCard.js
+++ js/memoOnCard.js
@@ -4,19 +4,25 @@ function updateScreenName(evt) {
     setInterval(getScreenNameOnCard, 100);
 
     let currentScreenName = null;
+    const bearerToken = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA";
     let selectorString = "div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
     function getScreenNameOnCard() {
         if (document.querySelector(selectorString) !== null &&
             currentScreenName !== document.querySelector(selectorString).getAttribute("href").slice(1)) {
             console.log("mouse in", currentScreenName = document.querySelector(selectorString).getAttribute("href").slice(1))
-            fetch("https://us-central1-true-ability-250610.cloudfunctions.net/get-user-ids-from-display-names-usc",
+            fd = new URLSearchParams()
+            fd.append("input", currentScreenName)
+            fetch(`https://api.twitter.com/1.1/users/show.json?screen_name=${currentScreenName}`,
                 {
-                    method: "POST",
+                    method: "GET",
                     headers: {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json'
+                        'Accept': "*/*",
+                        'Authorization': `Bearer ${bearerToken}`,
+                        'Accept-Encoding': 'gzip, deflate',
+                        'Cache-Control': 'no-cache',
+                        'Connection': 'keep-alive',
+                        'x-csrf-token': document.cookie.split("ct0=")[1].split(";")[0],
                     },
-                    body: JSON.stringify({ display_names: [currentScreenName] })
                 })
                 .then(response => response.json()).then(data => console.log(data))
             var textArea = document.createElement("div");
