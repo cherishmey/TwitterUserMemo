775cf26724ccb4db3abf1f0b49015241be1edfd6
rocky112358 <rocky112358@gmail.com>
2020-01-13 22:39:30 +0900

    FIX: bypass CORB and fix sturcture

--- js/memoOnCard.js
+++ js/memoOnCard.js
@@ -1,53 +1,82 @@
-window.addEventListener("load", updateScreenName, false);
+window.addEventListener("load", createMemoOnCard, false);
 
-function updateScreenName(evt) {
-    setInterval(getScreenNameOnCard, 100);
-
-    let currentScreenName = null;
-    const bearerToken = "AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA";
+function createMemoOnCard(evt) {
     let selectorString = "div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
-    function getScreenNameOnCard() {
-        if (document.querySelector(selectorString) !== null &&
-            currentScreenName !== document.querySelector(selectorString).getAttribute("href").slice(1)) {
-            console.log("mouse in", currentScreenName = document.querySelector(selectorString).getAttribute("href").slice(1))
-            fd = new URLSearchParams()
-            fd.append("input", currentScreenName)
-            fetch(`https://api.twitter.com/1.1/users/show.json?screen_name=${currentScreenName}`,
-                {
-                    method: "GET",
-                    headers: {
-                        'Accept': "*/*",
-                        'Authorization': `Bearer ${bearerToken}`,
-                        'Accept-Encoding': 'gzip, deflate',
-                        'Cache-Control': 'no-cache',
-                        'Connection': 'keep-alive',
-                        'x-csrf-token': document.cookie.split("ct0=")[1].split(";")[0],
-                    },
-                })
-                .then(response => response.json())
-                .then(data => {
-                    key = data.id_str
-                    chrome.storage.sync.get([key], // one key at a time
-                        function (result) {
-                            if (result[key] !== undefined) {
-                                var tag = result[key].tag
-                                var memo = result[key].memo
-                            } else {
-                                tag = ""
-                                memo = ""
-                            }
-                            console.log(key + ": { memo: " + memo + ", tag: " + tag + "}");
-                        }
-                    )
-                }
-                );
-            var textArea = document.createElement("div");
+
+    setInterval(createTextArea, 100);
+    function createTextArea() {
+        if (document.getElementById("memoCardInput") === null && document.querySelector(selectorString) !== null) { // card appeared but no memoInput
+            let textArea = document.createElement("div");
             textArea.setAttribute("id", "memoDiv")
             textArea.innerHTML = "<textarea id='memoCardInput'>"
             document.querySelector(selectorString).insertAdjacentElement("afterEnd", textArea);
-        } else if (document.querySelector(selectorString) === null && currentScreenName !== null) {
-            currentScreenName = null;
-            console.log("mouse out")
+            let screenName = getScreenNameOnCard()
+            if (screenName === null) {
+                console.log("Selector string has changed. Please contact developers.")
+            } else {
+                getUserIDInCard(screenName).then(userId => {
+                    console.log("userId: " + userId)
+                    loadCardData(userId) // and fill data
+                });
+
+                // and add an event listener
+                document.getElementById("memoCardInput").addEventListener("focusout", function () {
+                    let note = {}
+
+                    getUserIDInCard(screenName).then(userID => {
+                        let value = document.getElementById("memoCardInput").value;
+                        note[userID] = { "memo": value };
+                        console.log(note)
+                        saveCardData(note)
+                    });
+                });
+            }
         }
     }
+}
+function getScreenNameOnCard() {
+    let selectorString = "div.css-1dbjc4n.r-1r5jyh0.r-1ipicw7 div.css-1dbjc4n.r-1oqcu8e div.css-1dbjc4n.r-18u37iz.r-1wtj0ep a[href]"
+    if (document.querySelector(selectorString) !== null) {
+        return document.querySelector(selectorString).getAttribute("href").slice(1);
+    } else {
+        return null;
+    }
+}
+
+function getUserIDInCard(currentScreenName) {
+    return new Promise((resolve, reject) => {
+        chrome.runtime.sendMessage(
+            {
+                contentScriptQuery: "getUserId",
+                screenName: currentScreenName,
+                csrfToken: document.cookie.split("ct0=")[1].split(";")[0]
+            },
+            id_str => resolve(id_str))
+    });
+}
+
+function saveCardData(items) {
+    chrome.storage.sync.set(items, function (items) {
+    });
+}
+
+function loadCardData(key) {
+    chrome.storage.sync.get([key], function (result) {
+        let tag = '';
+        let memo = '';
+        try {
+            memo = result[key].memo
+        }
+        catch (TypeError) {
+            memo = ''
+        }
+        try {
+            tag = result[key].tag
+        }
+        catch (TypeError) {
+            tag = ''
+        }
+        document.getElementById("memoCardInput").value = memo
+        console.log(`${key}: { memo: ${memo}, tag: ${tag}}`);
+    });
 }
