d26f242d80dfd133f7ce04426777781b38f4a88b
rocky112358 <rocky112358@gmail.com>
2020-03-29 15:51:53 +0900

    DEL: remove permission declarativeContent

--- js/background.js
+++ js/background.js
@@ -1,21 +1,3 @@
-chrome.runtime.onInstalled.addListener(function () {
-    // Replace all rules ...
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        // With a new rule ...
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [
-                    new chrome.declarativeContent.PageStateMatcher({
-                        pageUrl: { hostEquals: 'twitter.com', schemes: ['https'] },
-                    })
-                ],
-                // And shows the extension's page action.
-                actions: [new chrome.declarativeContent.ShowPageAction()]
-            }
-        ]);
-    });
-});
-
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.contentScriptQuery == "getUserId") {
